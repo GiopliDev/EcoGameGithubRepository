ae64fc175466c001dc88642fd800364ff8b7d05c
LManeL26 <manentileonardo06@gmail.com>
2023-11-13 10:48:27 +0100

    Ora può anche cambiare il nome della cartella

--- download.bat
+++ download.bat
@@ -2,4 +2,11 @@
 git clone "https://github.com/GiopliDev/EcoGameGithubRepository"
 if "%1" neq "" (
 	move ./EcoGameGithubRepository ./%1
+	goto eof
 )
+set file=
+set /p "folder=Folder Name (EMPTY to get default `EcoGameGithubRepository`): "
+if "%folder%" EQU "" (
+	goto eof
+)
+	
