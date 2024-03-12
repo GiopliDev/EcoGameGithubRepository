c2d19639cc00ac0441daa1985e558fc788ea66b8
FranciMiglio06 <migliorucci.francesco@franchettisalviani.net>
2024-03-12 22:45:24 +0100

    Sistemato problema oggetti raccoglibili

--- Assets/Script/PickableItem.cs
+++ Assets/Script/PickableItem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PickableItem : MonoBehaviour
@@ -8,17 +9,30 @@ public class PickableItem : MonoBehaviour
 
     public InventoryManager inventoryManager;
 
+    public float moveSpeed; // Velocità di movimento dell'oggetto
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
-        { 
-            var playerPosition=Camera.main.ScreenToWorldPoint(collision.transform.position);
-            this.gameObject.transform.Translate(playerPosition);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(MoveObject(collision.gameObject.transform.position));
+        }
+    }
+
+    IEnumerator MoveObject(Vector3 targetPosition)
+    {
+        float distance = Vector3.Distance(transform.position, targetPosition); // Calcola la distanza tra l'oggetto e la posizione del giocatore
+        float step = moveSpeed * Time.deltaTime; // Calcola la quantità di movimento per frame
+
+        while (transform.position != targetPosition)
+        {
+            // Sposta l'oggetto verso la posizione del giocatore in modo graduale
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+            yield return null; // Attendere fino al prossimo frame
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Collide");
         inventoryManager.AddItem(item);
         Destroy(gameObject);
     }
